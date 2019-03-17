d3b4e1b762fb22890a587a1d0a60e3bac88a585a
MONDA <mondari@qq.com>
2019-03-17 21:02:38 +0800

    提交spring-boot-docker

---
+++ spring-boot-docker/README.md
@@ -0,0 +1,2 @@
+# spring-boot-docker
+参考 [Spring Boot with Docker](https://spring.io/guides/gs/spring-boot-docker/)
---
+++ spring-boot-docker/src/main/java/com/mondari/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.mondari;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(DockerApplication.class.getPackage().getName() + ".controller"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 页面标题
+                .title("Spring Boot Docker")
+                .build();
+    }
+}
