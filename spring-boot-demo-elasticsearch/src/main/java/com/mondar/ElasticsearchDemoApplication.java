19325a6a120b81f2cf160846e18d6a7d361107dc
limengda <limengda@qq.com>
2019-11-06 19:38:28 +0800

    1.添加JSON依赖；2.添加Elasticsearch更新操作；3.添加Apache License；

--- spring-boot-demo-elasticsearch/src/main/java/com/mondar/ElasticsearchDemoApplication.java
+++ spring-boot-demo-elasticsearch/src/main/java/com/mondar/ElasticsearchDemoApplication.java
@@ -1,5 +1,9 @@
 package com.mondar;
 
+import com.alibaba.fastjson.JSON;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +12,8 @@ import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.springframework.data.elasticsearch.core.query.Criteria;
 import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.UpdateQuery;
+import org.springframework.data.elasticsearch.core.query.UpdateQueryBuilder;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -29,19 +35,38 @@ public class ElasticsearchDemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        // 查询
+        System.out.println("————————查询————————");
         String expectedDate = "2014-10-29";
         String expectedWord = "java";
         CriteriaQuery query = new CriteriaQuery(
                 new Criteria("keywords").contains(expectedWord).and(new Criteria("date").greaterThanEqual(expectedDate)));
-
         List<Conference> conferenceList = restTemplate.queryForList(query, Conference.class);
+        conferenceList.forEach(conference -> {
+            System.out.println(JSON.toJSONString(conference, true));
+        });
 
+        // 更新
+        System.out.println("————————更新————————");
+        conferenceList.forEach(conference -> {
+            conference.setName(conference.getName().concat(" is a complete success"));
+            UpdateRequest doc = new UpdateRequest().doc(JSON.toJSONString(conference), XContentType.JSON);
+            UpdateQuery updateQuery = new UpdateQueryBuilder()
+                    .withClass(Conference.class)
+                    .withId(String.valueOf(conference.getId()))
+                    .withUpdateRequest(doc)
+                    .build();
+            UpdateResponse update = restTemplate.update(updateQuery);
+            System.out.println(JSON.toJSONString(update, true));
+        });
+
+        // 删除索引
         deleteIndex();
     }
 
     @PostConstruct
     public void insertDataSample() {
-
+        // 删除所有的文档
         repository.deleteAll();
 
         // Save data sample
