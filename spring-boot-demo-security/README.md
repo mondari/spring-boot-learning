d6ac63d6027ca7603c2c15219297142b4b944010
limengda <mondari@qq.com>
2020-03-01 18:58:57 +0800

    添加spring-security-demo

---
+++ spring-boot-demo-security/README.md
@@ -0,0 +1,34 @@
+# spring-boot-demo-security
+
+[toc]
+
+## 两种认证方式
+
+### 表单登录认证
+
+浏览器表单登录页面：
+
+![image-20200301160507701](README.assets/image-20200301160507701.png)
+
+浏览器请求如图：
+
+![image-20200301170323993](README.assets/image-20200301170323993.png)
+
+通过Postman表单登录：
+
+![image-20200301170037015](README.assets/image-20200301170037015.png)
+
+### HTTP Basic 认证
+
+浏览器认证界面：
+
+（待完善）
+
+Postman认证方法：
+
+![image-20200301171444373](README.assets/image-20200301171444373.png)
+
+注意是GET请求而不是POST，路径是 /hello 而不是 /login
+
+参考：https://blog.csdn.net/wangb_java/article/details/86502166
+
---
+++ spring-boot-demo-security/src/main/java/com/mondari/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.mondari;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.io.PrintWriter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("usr")
+                // 密码是：user
+                .password("$2a$10$/tsDoL7LZTBeLvSe4pK0aebam8rUbLq3ERGM2DndqY0DCzsP342zK").roles("USER").and()
+                .withUser("adm")
+                // 密码是：admin
+                .password("$2a$10$yP2.KHlRLUolHRJGJb.afekqyB4bxZEpa06dNB7OXwh55i8eRA5ym").roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // 1-> 请求授权
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/**").hasRole("USER")
+                .anyRequest().authenticated() // 剩余其它接口，登录之后就能访问
+                .and()
+                // 2-> 配置表单登录认证
+                .formLogin()
+                .usernameParameter("username")// 定义登录时用户名的key，默认是username
+                .passwordParameter("password")// 定义登陆时密码的key，默认是password
+                .loginPage("/login")// 登录页面，会将表单提交到 loginProcessingUrl
+                .loginProcessingUrl("/login")// 默认同 loginPage
+                // 登录成功操作
+                .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                    PrintWriter out = httpServletResponse.getWriter();
+                    out.write("login success");
+                    out.flush();
+                })
+                // 登录失败操作
+                .failureHandler((httpServletRequest, httpServletResponse, e) -> {
+                    httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                    PrintWriter out = httpServletResponse.getWriter();
+                    out.write("login failure");
+                    out.flush();
+                })
+                .and()
+                // 3-> 配置注销
+                .logout()
+                .logoutUrl("/logout")// 默认值
+                // .logoutSuccessUrl("/login?logout")// 默认值
+                // 注销成功操作(设置后，上面的 logoutSuccessUrl 设置会被忽略）
+                .logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                    PrintWriter out = httpServletResponse.getWriter();
+                    out.write("logout success");
+                    out.flush();
+                })
+                .and()
+                // 4-> 配置 HTTP Basic 认证
+                .httpBasic()
+                .and()
+                // 5->  关闭csrf。踩坑记录：不关闭postman测试登录会失败
+                .csrf().disable()
+        ;
+
+    }
+
+}
