db484379f412aba936aa05b07d97bbf81de7090b
limengda <mondari@qq.com>
2020-11-01 12:32:30 +0800

    去掉Swagger，更改响应结果以支持测试多节点部署情况

--- spring-boot-docker/src/main/java/com/mondari/DockerApplication.java
+++ spring-boot-docker/src/main/java/com/mondari/DockerApplication.java
@@ -5,13 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @RestController
 public class DockerApplication {
 
     @RequestMapping("/")
-    public String hello() {
-        return "Hello Docker World";
+    public String hello(ServletRequest httpRequest) {
+        String localhost;
+        try {
+            localhost = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            localhost = "localhost";
+        }
+        return "Hello Docker World\n\n——from " + localhost + ":" + httpRequest.getLocalPort();
     }
 
     public static void main(String[] args) {
